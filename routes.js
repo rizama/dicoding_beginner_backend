8b4ad09811fcb953fc533878e5e7a5f6df95c468
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-04-17 13:05:26 +0700

    + hapi framework

---
+++ routes.js
@@ -0,0 +1,60 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/users/{username?}',
+        handler: (request, h) => {
+            console.log(h);
+            const { username = 'Anonimous' } = request.params;
+            const { lang } = request.query;
+            if (lang == "id") {
+                return `Hai, ${username}!`;
+            }
+            return `Hello, ${username}!`;
+        },
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}!`;
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    }
+];
+
+module.exports = routes;
