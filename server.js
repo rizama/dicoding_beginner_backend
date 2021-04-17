464f22458b9e91485d67676464717a6f78df23b4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-04-17 11:37:59 +0700

    ^Response COde

--- server.js
+++ server.js
@@ -8,20 +8,23 @@ const http = require("http");
  */
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
 
     const { method, url } = request;
 
     if (url === '/') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if (url === '/about') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about</h1>');
         } else if (method === 'POST') {
+            response.statusCode = 200;
             let body = [];
 
             request.on('data', (chunk) => {
@@ -34,9 +37,11 @@ const requestListener = (request, response) => {
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
